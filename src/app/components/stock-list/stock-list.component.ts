29dd9f8c6283b5d7350cc3038b96e433a694f28e
Brian Batista <brian.batista@bradesco.com.br>
2020-10-15 15:30:17 -0300

    avatar

--- src/app/components/stock-list/stock-list.component.ts
+++ src/app/components/stock-list/stock-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-stock-list',
@@ -7,10 +7,15 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class StockListComponent implements OnInit {
   @Input() titulo: string;
+  @Output() onClick: EventEmitter<any> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public clicked() {
+    this.onClick.emit();
   }
 
 }
